5883fa19a84b1640c43077eedd14ae18b73c4b7b
KAAAsS <admin@kaaass.net>
2019-10-23 20:08:09 +0800

    完成静态资源上传，测试

--- src/main/java/net/kaaass/kmall/controller/ResourceController.java
+++ src/main/java/net/kaaass/kmall/controller/ResourceController.java
@@ -3,20 +3,30 @@ package net.kaaass.kmall.controller;
 import net.kaaass.kmall.dao.entity.MediaEntity;
 import net.kaaass.kmall.dao.repository.MediaRepository;
 import net.kaaass.kmall.dto.MediaDto;
+import net.kaaass.kmall.exception.BadRequestException;
 import net.kaaass.kmall.mapper.CommenMapper;
 import net.kaaass.kmall.util.Constants;
+import net.kaaass.kmall.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/resource")
-@PreAuthorize("authenticated()")
+@PreAuthorize("isAuthenticated()")
 public class ResourceController extends BaseController {
+
+    @Value("${file.uploadFolder}")
+    private String uploadFolder;
 
     @Autowired
     private MediaRepository mediaRepository;
@@ -37,6 +47,29 @@ public class ResourceController extends BaseController {
         entity.setType(type);
         entity.setUrl(url);
         entity.setUploaderUid(getUid());
+        var result = mediaRepository.save(entity);
+        return CommenMapper.INSTANCE.mediaEntityToDto(result);
+    }
+
+    @PutMapping("/image/")
+    public MediaDto uploadImage(@RequestParam MultipartFile file) throws BadRequestException, IOException {
+        if (file.isEmpty()) {
+            throw new BadRequestException("无文件传送！");
+        }
+
+        var originalFilename = file.getOriginalFilename();
+        var suffix = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+        var newFileName = StringUtils.uuid() + "." + suffix;
+        // TODO 检查suffix
+
+        File destFile = new File(uploadFolder + newFileName);
+        file.transferTo(destFile);
+
+        var entity = new MediaEntity();
+        entity.setType(Constants.MEDIA_TYPE_IMAGE);
+        entity.setUrl("/upload/" + newFileName);
+        entity.setUploaderUid(getUid());
+
         var result = mediaRepository.save(entity);
         return CommenMapper.INSTANCE.mediaEntityToDto(result);
     }
--- src/main/java/net/kaaass/kmall/util/StringUtils.java
+++ src/main/java/net/kaaass/kmall/util/StringUtils.java
@@ -1,13 +1,16 @@
 package net.kaaass.kmall.util;
 
+import org.springframework.util.ClassUtils;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 public class StringUtils {
 
     public static String uuid() {
-        return UUID.randomUUID().toString();
+        return UUID.randomUUID().toString().replace("-", "");
     }
 
     public static String orderId(String lastOrderId) {
@@ -19,5 +22,9 @@ public class StringUtils {
         var oldNum = Integer.valueOf(oldOrderNumStr);
         var orderNum = String.format("%04d", oldNum + 1);
         return dateStr + orderNum;
+    }
+
+    public static String resourcePath() {
+        return Objects.requireNonNull(Objects.requireNonNull(ClassUtils.getDefaultClassLoader()).getResource("")).getPath();
     }
 }
