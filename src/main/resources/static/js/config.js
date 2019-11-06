90319a43c3f9818e5bc4f63a125f2b1878385529
KAAAsS <admin@kaaass.net>
2019-11-06 22:12:26 +0800

    更换Axios的cdn

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -10,7 +10,7 @@ requirejs.config({
         }
     },
     paths: {
-        axios: "https://unpkg.com/axios/dist/axios.min",
+        axios: "https://cdn.bootcss.com/axios/0.19.0/axios.min",
         handlebars: "https://cdn.bootcss.com/handlebars.js/4.4.2/handlebars.min",
         bootstrap: "https://v4.bootcss.com/docs/4.3/dist/js/bootstrap.bundle",
         jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.slim.min"
