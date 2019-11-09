2090f82596a9d08d58b7e7a713c8af56b53be961
KAAAsS <admin@kaaass.net>
2019-11-09 11:54:02 +0800

    修正若干处页面样式问题

---
+++ src/main/resources/static/js/my/settings.js
@@ -0,0 +1,11 @@
+/**
+ * 商品详细页面
+ */
+require([
+    'jquery',
+    'module/functions',
+    'module/constants',
+    'module/auth',
+    'bootstrap'], function ($, functions, constants, auth, _) {
+    // TODO Add something
+});
