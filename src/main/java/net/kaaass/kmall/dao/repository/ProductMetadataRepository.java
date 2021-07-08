1770215e8383244515bb0031ac145a6d8b8dbbfd
KAAAsS <admin@kaaass.net>
2021-07-09 02:51:30 +0800

    规格参数修改

--- src/main/java/net/kaaass/kmall/dao/repository/ProductMetadataRepository.java
+++ src/main/java/net/kaaass/kmall/dao/repository/ProductMetadataRepository.java
@@ -2,8 +2,10 @@ package net.kaaass.kmall.dao.repository;
 
 import net.kaaass.kmall.dao.entity.ProductEntity;
 import net.kaaass.kmall.dao.entity.ProductMetadataEntity;
+import net.kaaass.kmall.dao.entity.ProductTemplateEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,4 +17,7 @@ public interface ProductMetadataRepository extends JpaRepository<ProductMetadata
     List<ProductMetadataEntity> findAllByProductId(String productId);
 
     List<ProductMetadataEntity> findAllByKeyAndValueLike(String key, String valueLike, Pageable pageable);
+
+    @Query("select c from ProductMetadataEntity c join ProductEntity p on c.productId = p.id where c.key = :key and p.category.template = :template")
+    List<ProductMetadataEntity> findAllByKeyAndTemplate(String key, ProductTemplateEntity template);
 }
--- src/main/java/net/kaaass/kmall/service/impl/ProductTemplateServiceImpl.java
+++ src/main/java/net/kaaass/kmall/service/impl/ProductTemplateServiceImpl.java
@@ -1,9 +1,12 @@
 package net.kaaass.kmall.service.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import net.kaaass.kmall.controller.request.ProductTemplateRequest;
 import net.kaaass.kmall.dao.entity.ProductTemplateEntity;
 import net.kaaass.kmall.dao.repository.CategoryRepository;
+import net.kaaass.kmall.dao.repository.ProductMetadataRepository;
 import net.kaaass.kmall.dao.repository.ProductTemplateRepository;
 import net.kaaass.kmall.dto.ProductTemplateDto;
 import net.kaaass.kmall.exception.NotFoundException;
@@ -11,10 +14,15 @@ import net.kaaass.kmall.mapper.EntityCreator;
 import net.kaaass.kmall.mapper.PojoMapper;
 import net.kaaass.kmall.service.CategoryService;
 import net.kaaass.kmall.service.ProductTemplateService;
+import net.kaaass.kmall.util.Constants;
+import net.kaaass.kmall.vo.ActualTemplateVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,6 +45,12 @@ public class ProductTemplateServiceImpl implements ProductTemplateService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductMetadataRepository metadataRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Override
     public List<ProductTemplateDto> getAll(Pageable pageable) {
         return repository.findAll(pageable).stream()
@@ -57,8 +71,48 @@ public class ProductTemplateServiceImpl implements ProductTemplateService {
         var editEntity = entityCreator.createProductTemplateEntity(request);
         editEntity.setId(entity.getId());
         editEntity.setCreateTime(entity.getCreateTime());
-        var result = repository.save(editEntity);
-        return pojoMapper.entityToDto(result);
+        var newEntity = repository.save(editEntity);
+        var result = pojoMapper.entityToDto(newEntity);
+        // 同步格式到所有元数据
+        var schemas = result.getSchema();
+        var allTemplates = metadataRepository.findAllByKeyAndTemplate(Constants.METAKEY_TEMPLATE, newEntity);
+        for (var metadata : allTemplates) {
+            try {
+                // 反序列化实际模板
+                var strTemplate = metadata.getValue();
+                List<ActualTemplateVo> template = objectMapper.readValue(strTemplate, new TypeReference<List<ActualTemplateVo>>(){});
+                // 创建新模板
+                var newTemplate = new ArrayList<ActualTemplateVo>();
+                for (var schema : schemas) {
+                    var actual = new ActualTemplateVo();
+                    var group = schema.getGroup();
+                    // 根据结构生成新数据
+                    actual.setGroup(group);
+                    var columns = new HashMap<String, String>(schema.getColumns().size());
+                    actual.setColumns(columns);
+                    schema.getColumns().forEach(el -> columns.put(el, ""));
+                    // 找到之前的数据
+                    var before = template.stream()
+                            .filter(el -> el.getGroup().equals(group))
+                            .findAny();
+                    // 遍历老的map，设置值
+                    before.ifPresent(old -> {
+                        old.getColumns().forEach((oldKey, oldValue) -> {
+                            // 如果新的字段仍然存在，设置老的值
+                            columns.computeIfPresent(oldKey, (s, s2) -> oldValue);
+                        });
+                    });
+                    // 添加
+                    newTemplate.add(actual);
+                    // 序列化新的模板，保存
+                    strTemplate = objectMapper.writeValueAsString(newTemplate);
+                    metadata.setValue(strTemplate);
+                }
+            } catch (IOException ignored) {
+            }
+        }
+        metadataRepository.saveAll(allTemplates);
+        return result;
     }
 
     @Override
